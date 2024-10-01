83de440246533ebadf21a0e9435e6e43dff47227
Braden Markievich <166034476+Braden-Markievich@users.noreply.github.com>
2024-10-01 17:25:54 -0400

    Update CF%_TOI.py

--- CF%_TOI.py
+++ CF%_TOI.py
@@ -8,7 +8,7 @@ toi = df['Weighted EV TOI'] * 500
 plt.figure(figsize=(10, 6))
 scatter = plt.scatter(players, cf_rel, s=toi, c=cf_rel, cmap='coolwarm', alpha=0.6)
 cbar = plt.colorbar(scatter)
-cbar.set_label('Even Strength CF% Rel', rotation = 90, fontsize=12) #labelpad = 55)
+cbar.set_label('Even Strength CF% Rel', rotation = 90, fontsize=12)
 plt.title('Measuring Even Strength CF% Rel and Weighted Even Strength TOI of the 2023-24 Washington Capitals', fontsize=16) 
 plt.xlabel('Players', fontsize=12)
 plt.ylabel('Even Strength CF% Rel', fontsize=12)
